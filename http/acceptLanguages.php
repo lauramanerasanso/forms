2c291f694357ac4d1924dda1f65ea3d1298f8230
Laura Manera Sansó <lauramanerasanso@MacBook-MacBook-Pro-de-Laura-Manera-Sanso.local>
2020-10-29 15:47:49 +0100

    second commit

---
+++ http/acceptLanguages.php
@@ -0,0 +1,17 @@
+<?php
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+switch ($lang){
+    case "es":
+        echo "<h1>Bienvenido a la página!</h1>";
+        break;
+    case "ca":
+        echo "<h1>Benvingut a la pàgina!</h1>";
+        break;
+    case "en":
+        echo "<h1>Welcome to the page!</h1>";
+        break;
+    default:
+        echo "<h1>Benvingut a la pàgina!</h1>";
+        break;
+}
+?>
