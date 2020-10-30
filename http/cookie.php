968f9a5e7322061e3ad543d386f40aefb9493639
lauramanerasanso <lauraams99@gmail.com>
2020-10-30 12:00:39 +0100

    nove commit

--- http/cookie.php
+++ http/cookie.php
@@ -3,7 +3,7 @@
         echo "Has visitat la pàgina un total de " .$_COOKIE['countVisits']." vegades en el darrer any.";
         setcookie('countVisits', $_COOKIE['countVisits']+1, time()+365*24*60*60);
     } else {
-        setcookie('countVisits', 1, time()+365*24*60*60);
-        echo "Has visitat la pàgina un total de " .$_COOKIE['countVisits']." vegades en el darrer any.";
+        setcookie('countVisits', 0, time()+365*24*60*60);
+        echo "Benvingut a la pàgina. Has visitat la pàgina un total de " .$_COOKIE['countVisits']." vegades en el darrer any.";
     }
 ?>
