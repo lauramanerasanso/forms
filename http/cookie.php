d8ee52256f4e44ebe2232441c37994d032ce64a9
lauramanerasanso <lauraams99@gmail.com>
2020-10-30 11:56:45 +0100

    octau commit

--- http/cookie.php
+++ http/cookie.php
@@ -1,8 +1,7 @@
 <?php
     if(isset($_COOKIE['countVisits'])){
-        setcookie('countVisits', $_COOKIE['countVisits']+1, time()+365*24*60*60);
         echo "Has visitat la pàgina un total de " .$_COOKIE['countVisits']." vegades en el darrer any.";
-
+        setcookie('countVisits', $_COOKIE['countVisits']+1, time()+365*24*60*60);
     } else {
         setcookie('countVisits', 1, time()+365*24*60*60);
         echo "Has visitat la pàgina un total de " .$_COOKIE['countVisits']." vegades en el darrer any.";
